4451361cafe232d060d7b2333a5894544d6e9e48
RHYTHM-09 <86103300+RHYTHM-09@users.noreply.github.com>
2021-08-24 20:24:09 +0530

    Add files via upload

---
+++ Project_Template_24-main/js/Player.js
@@ -0,0 +1,42 @@
+class Player {
+  constructor(x, y, width, height) {
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height;
+
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    World.add(world, this.body);
+  }
+
+ 
+  display() {
+
+    if (keyIsDown(RIGHT_ARROW) && this.angle < 0.35) {
+      this.angle += 0.02;
+    }
+
+    if (keyIsDown(LEFT_ARROW) && this.angle > -1.45) {
+      this.angle -= 0.02;
+    }
+
+    var pos = this.body.position;
+    var angle = this.body.angle;
+  fill("#676e6a");
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
